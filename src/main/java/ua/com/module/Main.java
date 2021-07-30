12863edfa24abade6c375b448b2516461668ffbc
Sheblyk <kokosigel@gmail.com>
2021-07-30 15:26:10 +0300

    added enterPoint to app

---
+++ src/main/java/ua/com/module/Main.java
@@ -0,0 +1,11 @@
+package ua.com.module;
+
+import ua.com.module.controller.Controller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
+    }
+}
